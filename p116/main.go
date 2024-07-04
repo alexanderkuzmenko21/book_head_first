58818540120c7c41e0191b54998463efbe0eea73
Alexander Kuzmenko <alexander.kuzmenko21prox@gmail.com>
2024-07-04 11:53:18 +0300

    task from page 116

---
+++ p116/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("A float: %f\n", 3.1415)
+	fmt.Printf("A float with rescricted numbers: %0.3f\n", 3.1415)
+	fmt.Printf("An integer: %d\n", 15)
+	fmt.Printf("A string: %s\n", "hello")
+	fmt.Printf("A boolean: %t\n", false)
+	fmt.Printf("Values: %v %v %v\n", 1.2, "\t", true)
+	fmt.Printf("Values: %#v %#v %#v\n", 1.2, "\t", true)
+	fmt.Printf("Types: %T %T %T\n", 1.2, "\t", true)
+	fmt.Printf("Percent sign: %%\n")
+}
